0035574ce203b4bab8b58aea96bd9b2c58c237dd
fjsabino022 <fjs_022@hotmail.com>
2017-09-16 10:19:16 -0300

    Agregado de carpeta seccion 3

---
+++ section3_intro-componentes/teoria.ts
@@ -0,0 +1,3 @@
+//COMPONENTES: son clases normales que tienen un decorador.
+//DIRECTIVAS ESTRUCTURALES: son expresiones que indican al HTML que acciones debe realizar.
+  //ej: *ng-if , *ng-for, etc
