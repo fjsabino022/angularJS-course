80f17b0f6cdc90665ebc72dc195b634517a6b5d2
franco.sabino <franco.sabino@tecso.coop>
2017-09-19 08:23:45 -0300

    Lecture 31. 1.HolaMundo

--- section3_intro-componentes/teoria.ts
+++ section3_intro-componentes/teoria.ts
@@ -1,3 +1,5 @@
 //COMPONENTES: son clases normales que tienen un decorador.
 //DIRECTIVAS ESTRUCTURALES: son expresiones que indican al HTML que acciones debe realizar.
   //ej: *ng-if , *ng-for, etc
+
+  
